2cda1ceac3559a58307ee0c9019be5b83edaf142
Gerhard Knapp <gerhard.knapp@ars.de>
2023-10-27 12:52:08 +0200

    reformat code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Initial fade-in for the page
     $('body').css('opacity', 0).animate({opacity: 1}, 1500);
 
     // Handle link clicks
-    $('a').on('click', function(event) {
+    $('a').on('click', function (event) {
         event.preventDefault();  // Prevent the default link behavior immediately
         var href = $(this).attr('href');
 
@@ -11,7 +11,7 @@ $(document).ready(function() {
         $('body').addClass('fade-out');
 
         // After the animation completes, navigate to the link
-        setTimeout(function() {
+        setTimeout(function () {
             window.location.href = href;
         }, 500);  // This delay should match the duration of the fade-out transition in the CSS
     });
